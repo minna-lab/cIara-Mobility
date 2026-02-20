3faf302c692266d8e70d628c3436585f4ffc81a0
Minna <zaide.assossou@gmail.com>
2026-02-20 10:45:15 +0100

    Afficher les locations avec le nom du client

---
+++ quetes/quetes_3/quete3_1.sql
@@ -0,0 +1,9 @@
+SELECT
+    location.id_location,
+    location.date_debut,
+    location.date_fin,
+    client.nom,
+    client.prenom
+FROM location
+JOIN client
+ON location.id_client = client.id_client;
